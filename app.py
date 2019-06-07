2451128056278df8d4b4b5da44c5dc3659386de9
saiyerniakhil <saiyerniakhil>
2019-06-07 15:10:06 +0530

    Add Register option

--- app.py
+++ app.py
@@ -7,6 +7,35 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route("/register")
+def register():
+    return render_template('result.html')
+
+@app.route("/regsuccess",methods=['POST',"GET"])
+def regsuccess():
+    if request.method == "POST":
+        conn = sqlite3.connect('login.db')
+        cur = conn.cursor()
+        
+        userid = str(request.form.get('email-phone'))
+        password = request.form.get('password')
+
+        try:
+            cur.execute('''
+                INSERT INTO logins VALUES (?,?)
+            ''',(userid,password))
+            res = {"Success":"registration success"}
+        except:
+            res = {"Error":"Failed to insert"}
+
+        conn.commit()
+
+        return render_template("success.html",data=res)
+    else:
+        res = {"Error":"Something wrong has happened"}
+        return render_template("success.html",data=res)
+
+
 @app.route("/success",methods=['POST',"GET"])
 def login():
     if request.method == "POST":
